7bd52b8906ddc5eb525eafea861957b91c3b732c
frostgaming <frostgaming@users.noreply.github.com>
2021-07-24 19:15:09 -0500

    Update backup.sh
    and some more.

--- directadmin-backup/backup.sh
+++ directadmin-backup/backup.sh
@@ -2,36 +2,36 @@ backupdir="/mnt/hdd/backups/"
 export dstr=$(date +"%m-%d-%y")
 
 gzip_user () {
-	user="$1"
-	echo "Running pigz for $user"
-	tar --use-compress-program=pigz -cf $backupdir/$dstr/$user/$user.tar.gz "/home/$user/"
+    user="$1"
+    echo "Running pigz for $user"
+    tar --use-compress-program=pigz -cf $backupdir/$dstr/$user/$user.tar.gz "/home/$user/"
 }
 
 mysql_backup () {
-	user="$1"
-	userdbs=$(mysql -e 'show databases' -s --skip-column-names | grep -wE "${user}_.*.")
+    user="$1"
+    userdbs=$(mysql -e 'show databases' -s --skip-column-names | grep -wE "${user}_.*.")
 
-	for db in $userdbs; do
-		echo "Backing up database $db"
-		mysqldump $db | gzip > $backupdir/$dstr/$user/databases/$db.sql.gz
-	done
+    for db in $userdbs; do
+        echo "Backing up database $db"
+        mysqldump $db | gzip > $backupdir/$dstr/$user/databases/$db.sql.gz
+    done
 }
 
 rotate_backups () {
     # 7 day backup retention
-	for dir in $(find /mnt/hdd/backups/ -type d -mtime +7); do
-		echo "Purging $dir"
-		rm -rf "$dir"
-	done
+    for dir in $(find /mnt/hdd/backups/ -type d -mtime +7); do
+        echo "Purging $dir"
+        rm -rf "$dir"
+    done
 }
 
 for user in $(find /usr/local/directadmin/data/users/ -maxdepth 1 -mindepth 1 -type d -printf '%f\n'); do
-	if [[ ! -d "$backupdir/$dstr/$user/databases/" ]]; then
-		mkdir -p "$backupdir/$dstr/$user/databases/"
-	fi
-	
-	gzip_user "$user"
-	mysql_backup "$user"
+    if [[ ! -d "$backupdir/$dstr/$user/databases/" ]]; then
+        mkdir -p "$backupdir/$dstr/$user/databases/"
+    fi
+
+    gzip_user "$user"
+    mysql_backup "$user"
 done
 
 rotate_backups
