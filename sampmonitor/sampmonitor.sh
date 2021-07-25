b46bb10d237fc794bd0bda37b37e5d00674430fb
frostgaming <frostgaming@users.noreply.github.com>
2021-07-24 19:14:35 -0500

    Fix more inconsistent tabs and spaces

--- sampmonitor/sampmonitor.sh
+++ sampmonitor/sampmonitor.sh
@@ -26,71 +26,71 @@ discord_bash_path="/home/rcrp/scripts/discord.sh"
 export DISCORD_WEBHOOK="redacted"
 
 function startsrv () {
-	if [[ ! -z "$pid" ]] && [[ $(ps -p "$pid" > /dev/null 2>&1; echo $?) -eq 0 ]]; then
-		echo "The server is already running with PID $pid"
-		exit 1
-	fi
+    if [[ ! -z "$pid" ]] && [[ $(ps -p "$pid" > /dev/null 2>&1; echo $?) -eq 0 ]]; then
+    	echo "The server is already running with PID $pid"
+    	exit 1
+    fi
 
-	if [[ "$1" != "--force" ]] && [[ -f "$lockfile" ]]; then
-		echo "$execn has been stopped intentionally! Add --force to override and start anyway."
-		exit 1
-	fi
+    if [[ "$1" != "--force" ]] && [[ -f "$lockfile" ]]; then
+    	echo "$execn has been stopped intentionally! Add --force to override and start anyway."
+    	exit 1
+    fi
 
-	sh $discord_bash_path \
-	--webhook-url=$WEBHOOK \
-	--username "Jimmy Shootsyabot" \
-	--author "Jimmy Shootsyabot" \
-	--description "$execn server process was not found and has been started." \
-	--color "0xDD3333" \
-	--footer "Jimmy Shootsyabot" \
-	--timestamp
+    sh $discord_bash_path \
+    --webhook-url=$WEBHOOK \
+    --username "Jimmy Shootsyabot" \
+    --author "Jimmy Shootsyabot" \
+    --description "$execn server process was not found and has been started." \
+    --color "0xDD3333" \
+    --footer "Jimmy Shootsyabot" \
+    --timestamp
 
-	su -c "cd $path; nohup ./$execn &" $user > /dev/null 2>&1
-	rm -f "$lockfile"
-	echo "$execn server has been started as user $user!"
+    su -c "cd $path; nohup ./$execn &" $user > /dev/null 2>&1
+    rm -f "$lockfile"
+    echo "$execn server has been started as user $user!"
 }
 
 function stopsrv () {
-	if [[ ! -z "$pid" ]] && [[ $(ps -p "$pid" > /dev/null 2>&1; echo $?) -eq 0 ]]; then
-		killtimer=0
-		touch $lockfile;
-		echo "Killing $execn with SIGINT..."
-		kill -2 "$pid" 
+    if [[ ! -z "$pid" ]] && [[ $(ps -p "$pid" > /dev/null 2>&1; echo $?) -eq 0 ]]; then
+        killtimer=0
+        touch $lockfile;
+        echo "Killing $execn with SIGINT..."
+        kill -2 "$pid" 
 
-		while [[ $(ps -p "$pid" > /dev/null 2>&1; echo $?) -eq 0 ]]; do
-			sleep 1
-			let "killtimer = killtimer + 1"
-			if [[ "$killtimer" -eq "$killtimeout" ]]; then
-				echo "Timeout reached for SIGINT on $execn. Killing forcefully..."
-				kill -9 "$pid"
-				break
-			fi
-		done
+        while [[ $(ps -p "$pid" > /dev/null 2>&1; echo $?) -eq 0 ]]; do
+            sleep 1
+            let "killtimer = killtimer + 1"
+            if [[ "$killtimer" -eq "$killtimeout" ]]; then
+                echo "Timeout reached for SIGINT on $execn. Killing forcefully..."
+                kill -9 "$pid"
+                break
+            fi
+        done
 	else
-		echo "No running process of $execn detected. Not killing."
-	fi
+        echo "No running process of $execn detected. Not killing."
+    fi
 }
 
 function displayusage () {
-	echo "You have not specified an argument!"
-	echo "Arguments: stop, start"
-	echo "Flags: --force"
+    echo "You have not specified an argument!"
+    echo "Arguments: stop, start"
+    echo "Flags: --force"
 }
 
 if [[ ! -d "$path" ]]; then
-	echo "Unable to locate folder $path"
-	exit 1
+    echo "Unable to locate folder $path"
+    exit 1
 fi
 
 case $1 in 
-	start)
-		startsrv "$2"
-	;;
-	stop)
-		stopsrv "$2"
-	;;
-	"")
-		displayusage
-		exit 1
-	;;
+    start)
+        startsrv "$2"
+    ;;
+    stop)
+        stopsrv "$2"
+    ;;
+    "")
+        displayusage
+        exit 1
+    ;;
 esac
